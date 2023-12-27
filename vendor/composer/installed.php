611b5639d9179cac71f02fca656d7027010a1d92
Aymane El Maini <121185649+AymaneTech@users.noreply.github.com>
2023-12-27 19:58:05 +0000

    good

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,23 +1,24 @@
-<?php return array(
-    'root' => array(
-        'name' => '__root__',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '99b721147ad593f431360377f8b872e29b071af1',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '99b721147ad593f431360377f8b872e29b071af1',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
+    'reference' => '51bb876c0f47d6ca554218fbc1d171ac62ccf05d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51bb876c0f47d6ca554218fbc1d171ac62ccf05d',
     ),
+  ),
 );
